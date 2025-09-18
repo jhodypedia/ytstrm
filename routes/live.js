62c96d57e4bd38e8ea3b765e03229889aff47e1c
jhodypedia <87615107+jhodypedia@users.noreply.github.com>
2025-09-19 02:08:01 +0700

    Update live.js

--- routes/live.js
+++ routes/live.js
@@ -2,7 +2,7 @@ import express from 'express';
 import multer from 'multer';
 import path from 'path';
 import fs from 'fs';
-import rimraf from 'rimraf';
+import { rimraf } from 'rimraf';
 
 import { requireAuth } from '../middleware/auth.js';
 import { createStreamAndBroadcast, setThumbnail } from '../services/youtube.js';
