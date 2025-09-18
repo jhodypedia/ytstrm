30d0a8af92ed1924d4616e870fe773a15b93677b
jhodypedia <87615107+jhodypedia@users.noreply.github.com>
2025-09-19 02:59:37 +0700

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -11,8 +11,8 @@ const getOAuth = () => new google.auth.OAuth2(
 
 const SCOPES = [
   'https://www.googleapis.com/auth/youtube',
-  'https://www.googleapis.com/auth/youtube.force-ssl',
-  'https://www.googleapis.com/auth/youtube.upload'
+  'https://www.googleapis.com/auth/youtube.upload',
+  'https://www.googleapis.com/auth/youtube.force-ssl'
 ];
 
 router.get('/login', (req, res) => {
